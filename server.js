138cbdd9d0541bbfb58064d89f861286775cee27
LuisNV <48541267+LuisNV@users.noreply.github.com>
2020-10-13 14:38:03 +0700

    update

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ mongoose.connect(DB,{
     useNewUrlParser:true
 }).then(()=>console.log('DB connection successful'));
 
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 3000;
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`))
